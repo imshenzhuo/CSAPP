f6887570db4fd69af8b2a9f279164a6da7eda696
imshenzhuo <im.shenzhuo@gmail.com>
2019-07-09 16:17:09 +0800

    add ch07 static link code

---
+++ code/ch07/foo2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int x = 1234;
+
+int main()
+{
+	return 0;
+}
---
+++ code/ch07/foo3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void f();
+int x = 1234;
+
+int main()
+{
+	f();
+	printf("%d\n", x);
+	return 0;
+}
